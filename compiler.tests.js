3692a09193bf3ca44c62a210ee434a08db8edc70
John Varga <john.varga@unifilabs.com>
2017-02-23 16:26:31 -0800

    write compiler tests

--- compiler.tests.js
+++ compiler.tests.js
@@ -3,9 +3,26 @@ const expect = chai.expect
 
 describe('compiler', () => {
 
+    let compiler = null
+
+    beforeEach(() => {
+        compiler = require('./compiler')
+    })
+
     it('should exist', () => {
-        let compiler = require('./compiler')
         expect(compiler).to.be.defined
+    })
+
+    it('should emit code for a simple program', () => {
+        let program = '(add 2 5)'
+        let compiled = compiler(program)
+        expect(compiled).to.equal('add(2, 5);')
+    })
+
+    it('should emit code for a complex program', () => {
+        let program = '(add 1 2 (mult 3 4))'
+        let compiled = compiler(program)
+        expect(compiled).to.equal('add(1, 2, mult(3, 4));')
     })
 
 })
