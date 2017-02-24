30e4e1c4fda6df16b85217fbc560c7e94c5d69a2
John Varga <john.varga@unifilabs.com>
2017-02-23 16:23:16 -0800

    write parser tests

--- parser.tests.js
+++ parser.tests.js
@@ -3,9 +3,68 @@ const expect = chai.expect
 
 describe('parser', () => {
 
+    let parser = null
+
+    beforeEach(() => {
+        parser = require('./parser')
+    })
+
     it('should exist', () => {
-        let parser = require('./parser')
         expect(parser).to.be.defined
     })
 
+    it('should parse a simple program', () => {
+        const tokens = [
+            { type: 'paren', value: '(' },
+            { type: 'name', value: 'add' },
+            { type: 'number', value: '2' },
+            { type: 'number', value: '3' },
+            { type: 'paren', value: ')' },
+        ]
+        const result = parser(tokens)
+        expect(result).to.deep.equal({
+            type: 'Program',
+            body: [{ 
+                type: 'CallExpression', 
+                name: 'add', 
+                params: [
+                    { type: 'NumberLiteral', value: '2' },
+                    { type: 'NumberLiteral', value: '3' }
+                ]
+            }]
+        })
+    })
+
+    it('should parse a nested program', () => {
+        const tokens =  [
+          { type: 'paren',  value: '(' },
+          { type: 'name',   value: 'add' },
+          { type: 'number', value: '2' },
+          { type: 'paren',  value: '(' },
+          { type: 'name',   value: 'subtract' },
+          { type: 'number', value: '4' },
+          { type: 'string', value: '314' },
+          { type: 'paren',  value: ')' }, 
+          { type: 'paren',  value: ')' }, 
+        ]
+        const result = parser(tokens)
+        expect(result).to.deep.equal({
+            type: 'Program',
+            body: [{
+                type: 'CallExpression',
+                name: 'add',
+                params: [
+                    { type: 'NumberLiteral', value: '2' },
+                    { 
+                        type: 'CallExpression', 
+                        name: 'subtract',
+                        params: [
+                            { type: 'NumberLiteral', value: '4' },
+                            { type: 'StringLiteral', value: '314' }
+                        ]
+                    }
+                ]
+            }]
+        })
+    })
 })
